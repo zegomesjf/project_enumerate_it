4afe881df6bb05beaa00660cb9b11da5777f4002
José Gomes Júnior <zegomesjf@gmail.com>
2013-03-17 01:35:04 -0300

    Corrige indentação

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,20 +2,20 @@ require "spec_helper"
 require "Person"
 
 describe Person do
-	context "Create Person with relationship status" do
-		before do
-			subject { Person.new }
-			subject.name = "Carlos Eduardo"
-		end
+  context "Create Person with relationship status" do
+    before do
+      subject { Person.new }
+      subject.name = "Carlos Eduardo"
+    end
 
-		it "have be Mr" do
-			subject.relationship_status = RelationshipStatus::MARRIED
-			expect(subject.married?).to be_true
-		end
+    it "have be Mr" do
+      subject.relationship_status = RelationshipStatus::MARRIED
+      expect(subject.married?).to be_true
+    end
 
-		it "have be Sr" do
-			subject.relationship_status = RelationshipStatus::SINGLE
-			expect(subject.single?).to be_true
-		end
-	end
+    it "have be Sr" do
+      subject.relationship_status = RelationshipStatus::SINGLE
+      expect(subject.single?).to be_true
+    end
+  end
 end
