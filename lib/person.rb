341d6568fd42795a39f11c048519758e19a285d0
José Gomes Júnior <zegomesjf@gmail.com>
2013-03-17 01:16:02 -0300

    Corrige indentação

--- lib/person.rb
+++ lib/person.rb
@@ -4,20 +4,16 @@ require "enumerate_it"
 require "relationship_status"
 
 class Person
-	extend EnumerateIt
-	attr_accessor :name, :relationship_status
+  extend EnumerateIt
+  attr_accessor :name, :relationship_status
 
-	has_enumeration_for :relationship_status, :with => RelationshipStatus, :create_helpers => true	
+  has_enumeration_for :relationship_status, :with => RelationshipStatus, :create_helpers => true
 
-	def pronoun_of_treatment
-		if self.married?
-			"Sr. #{name}"
-		else
-			"Mr. #{name}"
-		end
-	end
+  def pronoun_of_treatment
+    if self.married?
+      "Sr. #{name}"
+    else
+      "Mr. #{name}"
+    end
+  end
 end
-
-
-
-
